0756051a09f6fe43e9d191c587defac813b209f5
HE,XUAN-WEI <azrael8576love2008@gmail.com>
2020-06-11 11:25:26 +0800

    - Chore: dependencies stacked.

---
+++ lib/ui/views/login/login_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutterstacked/ui/views/login/login_viewmodel.dart';
+import 'package:stacked/stacked.dart';
+
+class LoginView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<LoginViewModel>.reactive(
+      builder: (context, model, child) => Scaffold(
+        body: Center(
+          child: Text(model.title),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: null),
+      ),
+      viewModelBuilder: () => LoginViewModel(),
+    );
+  }
+}
---
+++ lib/ui/views/login/login_viewmodel.dart
@@ -0,0 +1,6 @@
+import 'package:stacked/stacked.dart';
+
+class LoginViewModel extends BaseViewModel {
+  String _title = 'Login View';
+  String get title => _title;
+}
